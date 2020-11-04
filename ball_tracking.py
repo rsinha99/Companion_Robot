747b424af720d79cf39003c5c3047f086cf8b4e4
Nathan Hsu <NathanH.Hsu@gmail.com>
2020-11-03 20:27:12 -0500

    Modifies ball_tracking.py to include gstreamer

--- ball_tracking.py
+++ ball_tracking.py
@@ -24,10 +24,46 @@ greenLower = (29, 86, 6)
 greenUpper = (64, 255, 255)
 pts = deque(maxlen=args["buffer"])
 
+isNvidia = True
+
+# Get a reference to webcam #0 (the default one)
+def gstreamer_pipeline(
+		capture_width=1280,
+		capture_height=720,
+		display_width=1280,
+		display_height=720,
+		framerate=60,
+		flip_method=2,
+):
+	return (
+			"nvarguscamerasrc ! "
+			"video/x-raw(memory:NVMM), "
+			"width=(int)%d, height=(int)%d, "
+			"format=(string)NV12, framerate=(fraction)%d/1 ! "
+			"nvvidconv flip-method=%d ! "
+			"video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+			"videoconvert ! "
+			"video/x-raw, format=(string)BGR ! appsink"
+			% (
+				capture_width,
+				capture_height,
+				framerate,
+				flip_method,
+				display_width,
+				display_height,
+			)
+	)
+
+
 # if a video path was not supplied, grab the reference
 # to the webcam
 if not args.get("video", False):
-	camera = cv2.VideoCapture(0)
+
+	# To flip the image, modify the flip_method parameter (0 and 2 are the most common
+	if isNvidia:
+		camera = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+	else:
+		camera = cv2.VideoCapture(0)
 
 # otherwise, grab a reference to the video file
 else:
