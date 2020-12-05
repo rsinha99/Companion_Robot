bf84af2877594246ba6588289a948550290c34aa
Nathan Hsu <NathanH.Hsu@gmail.com>
2020-12-04 19:09:51 -0500

    Tests left and right movement

--- driver.py
+++ driver.py
@@ -18,15 +18,21 @@ def main():
 
     time.sleep(2)  # wait for Arduino to start up
     ser.flushInput()
-    # ser.write(b'\x03')  # Enter Calibration Mode
-    ser.write(b'\C3')
-    time.sleep(2)
 
-    while (True):
-        if ser.in_waiting > 0:
-            msg = ser.read(1)
-            print("Message from Arduino:")
-            print(hexlify(msg))
+    while True:
+        ser.write(b'\x40')
+        time.sleep(5)
+        ser.write(b'\x80')
+        time.sleep(5)
+
+    # ser.write(b'\x03')  # Enter Calibration Mode
+    # time.sleep(2)
+    #
+    # while True:
+    #     if ser.in_waiting > 0:
+    #         msg = ser.read(1)
+    #         print("Message from Arduino:")
+    #         print(hexlify(msg))
 
 
 if __name__ == "__main__":
