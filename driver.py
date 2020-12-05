fbfbdd3a9321581c0ec1270930c3465b5fa32803
bfehl <bfehl99@gmail.com>
2020-12-05 14:45:39 -0500

    Update driver.py

--- driver.py
+++ driver.py
@@ -3,28 +3,82 @@ import serial
 import time
 from binascii import hexlify
 
-isWindowsOS = False
 
-if isWindowsOS:
-    port = 'COM5'
-else:
-    port = '/dev/ttyUSB0'
 
 # TODO function to convert string to Serial Code for better user experience
 
 def main():
+    isWindowsOS = False
 
+    if isWindowsOS:
+        port = 'COM5'
+    else:
+        port = '/dev/ttyUSB0'
+
+    print("Hello")
     ser = serial.Serial(port=port, baudrate=9600, timeout=1)
 
     time.sleep(2)  # wait for Arduino to start up
     ser.flushInput()
 
-    ser.write(b'\x02')  # Enter Following Mode
-    while True:
+def forward():
+    main()
+    global following_mode
+    if following_mode = True:
+        ser.write(b'\x0')
+    if following_mode = False:
+        ser.write(b'\x02')
+        following_mode = True
+        ser.write(b'\x0')
+
+def right():
+    main()
+    global following_mode
+    if following_mode = True:
         ser.write(b'\x40')
-        time.sleep(5)
+    if following_mode = False:
+        ser.write(b'\x02')
+        following_mode = True
+        ser.write(b'\x40')
+
+def left():
+    main()
+    global following_mode
+    if following_mode = True:
         ser.write(b'\x80')
-        time.sleep(5)
+    if following_mode = False:
+        ser.write(b'\x02')
+        following_mode = True
+        ser.write(b'\x80')
+
+def dance():
+    main()
+    global following_mode
+    if following_mode = True:
+        while True:
+            ser.write(b'\x80')
+            time.sleep(5)
+            ser.write(b'\x40')
+            time.sleep(5)
+
+    if following_mode = False:
+        ser.write(b'\x02')
+        following_mode = True
+        while True:
+            ser.write(b'\x80')
+            time.sleep(5)
+            ser.write(b'\x40')
+            time.sleep(5)
+
+    #ser.write(b'\x02')  # Enter Following Mode
+    #print("Enter following mode")
+    #while True:
+     #   ser.write(b'\x40')
+     #   print("Left")
+     #   time.sleep(5)
+      #  ser.write(b'\x80')
+      #  print("Right")
+      #  time.sleep(5)
 
     # ser.write(b'\x03')  # Enter Calibration Mode
     # time.sleep(2)
