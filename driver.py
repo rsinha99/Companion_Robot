d83a5bd7b362efd8394eb9dce5580dc2118cb777
Nathan Hsu <NathanH.Hsu@gmail.com>
2020-12-08 05:29:15 -0500

    Establishes basic following and camera thread codes. Not tested

--- driver.py
+++ driver.py
@@ -3,6 +3,7 @@ import serial
 import time
 from threading import Thread, Event
 from binascii import hexlify
+import camera
 
 
 
@@ -76,12 +77,50 @@ def nav_test():
             print(hexlify(msg))
 
 
+def calibrate():
+    ser.write(b'\x03')
+    time.sleep(2)
+
+
+# TODO Modify the code to account for target being on the outer edges of the camera
+def follow_thread():
+    ser.write(b'\x02')          # Set robot to Following Mode
+    while True:
+        if event.is_set():
+            break
+
+        if camera.currX is None:    # No target to follow is detected
+            ser.write(b'\x46')      # Turn left until target is found
+            while camera.currX is None or camera.currX > 950 or camera.currX < 650:
+                None    # keep turning left until target is centered
+        elif camera.currX > 950:  # Color is to the right of robot
+            ser.write(b'\xE2')      # Drive Forward-right
+        elif camera.currX < 650:    # Target is to the left of robot
+            ser.write(b'\xC2')      # Drive Forward-left
+        else:
+            ser.write(b'\xC0')
+
+        # TODO if robot returns serial code "Obstacle", close this thread (break from loop)
+        print(camera.currX)
+        time.sleep(1)
+
 
 
 
 if __name__ == "__main__":
     print("starting...")
-    nav_test()
+    # color = Thread(target=camera.camera_thread)
+    # color.start()
+    # while True:
+    #     try:
+    #         if camera.currX is not None:
+    #             print(camera.currX)
+    #         time.sleep(1)
+    #     except (KeyboardInterrupt, Exception):
+    #         camera.event.set()
+    #         color.join()
+    #         break
+
     # COMMENTED OUT BELOW: USED FOR REFERENCE WHEN STARTING THREADS
     # read = Thread(target=thread_read)
     # write = Thread(target=thread_modify)
