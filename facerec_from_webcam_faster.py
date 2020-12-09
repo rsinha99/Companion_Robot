213a6b9fbb15e46924ede802fe076b660cb621d9
Nathan Hsu <NathanH.Hsu@gmail.com>
2020-12-08 19:07:32 -0500

    Bug fixes

--- facerec_from_webcam_faster.py
+++ facerec_from_webcam_faster.py
@@ -21,7 +21,7 @@ video_capture = cv2.VideoCapture(cameraNumber)
 # rohit_face_encoding = face_recognition.face_encodings(rohit_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-nathan_image = face_recognition.load_image_file("nathan.jpg")
+nathan_image = face_recognition.load_image_file("Nathan.jpg")
 nathan_face_encoding = face_recognition.face_encodings(nathan_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
