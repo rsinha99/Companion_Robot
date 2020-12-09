7e44a3002fd98dbe9bac649868eb265763e7b68a
bfehl <bfehl99@gmail.com>
2020-12-09 13:57:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from flask_ask import Ask, statement
-#import facerec_from_webcam_faster as facerec
+import facerec_from_webcam_faster as facerec
 import threading
 import driver
 import test_robot_chat_client as chat
 import numpy as np
 import serial
 import time
+import camera
 from binascii import hexlify
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+threads = []
 #following_mode = False
 #isWindowsOS = False
 
@@ -23,10 +25,17 @@ ask = Ask(app, '/')
 
 
 @ask.intent('WhoFace')
-
-#def who_face():
- #   speech_text = 'Hi beautiful, woops I mean {}'.format(facerec.name)
- #   return statement(speech_text).simple_card('Johnny', speech_text)
+def who_face():
+    print(facerec.name)
+    if facerec.name == "unknown":
+        print("in unkown")
+        speech_text = 'Where are you'
+    else:
+        print("in becca")
+        dance()
+        speech_text = 'Hi beautiful, woops I mean {}'.format(facerec.name)
+         
+    return statement(speech_text).simple_card('Johnny', speech_text)
 
 @ask.intent('Forward')
 def forward():
@@ -48,9 +57,19 @@ def right():
 
 @ask.intent('Follow')
 def follow_me():
-    driver.follow()
+    follow_thread = threading.Thread(target=driver.follow_thread, name="following_thread")
+    follow_thread.start()
+    threads.append(follow_thread)
+
     speech_text = 'Following'
     return statement(speech_text).simple_card('Johnny', speech_text)
+
+@ask.intent('StopFollow')
+def stop_follow():
+    driver.event.set()
+    for i in range(len(threads)):
+        if threads[i].name == "following_thread":
+            threads.pop(i).join()
 
 @ask.intent('Dance')
 def dance():
@@ -68,16 +87,24 @@ def stop():
     speech_text = 'Skrttt'
     return statement(speech_text).simple_card('Johnny', speech_text)
 
-@ask.intent('Party')
-def party():
-    chat()
+#@ask.intent('Party')
+#def party():
+#    chat()
 
-@ask.intent('Camera')
-def camera():
-  camera.camera_thread()
+
   
 
 if __name__== '__main__':
-    #face_rec = threading.Thread(target=facerec.facerec_thread)
-    #face_rec.start()
+    face_rec = threading.Thread(target=facerec.facerec_thread)
+    face_rec.start()
+    
+
+    cam_thread = threading.Thread(target=camera.camera_thread)
+    # face_rec_thread = Thread(target=face_rec.facerec_thread)
+    # face_rec_thread.start()
+    # threads.append(face_rec_thread)
+    cam_thread.start()
+    threads.append(cam_thread)
+    # forward()
+    # driver.forward()
     app.run()
